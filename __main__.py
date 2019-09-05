61c18636a57e78cf6091373f69c538566baef7e1
ThomasCloarec <thomas.cloarec.d@gmail.com>
2019-09-06 01:24:29 +0200

    Hypothesis now find the cost function local minimum

--- __main__.py
+++ __main__.py
@@ -2,28 +2,61 @@ import matplotlib.pyplot as plt
 import csv
 import random
 import math
+import os
 
 
 def main():
-    plt.ion()
-
-    # Defining default plot
-    plt.axis([0, 100, 0, 100])
+    # Define learning rate and epoch
+    learning_rate_a = 0.0001
+    learning_rate_b = 0.1
+    epoch = 100
 
     # Import dataset
     dataset = import_csv_data('train.csv')
 
-    # Scatter plot
-    for data_tuple in dataset:
-        plt.scatter(data_tuple[0], data_tuple[1])
+    # Initialize random parameters
+    a = math.tan(random.uniform(-math.pi / 2, math.pi / 2))
+    b = random.randint(0, 100)
 
-    for i in range(0, 1000):
-        plt.pause(0.05)
-        b = random.randint(0, 100)
-        a = math.tan(random.uniform(-math.pi / 2, math.pi / 2))
-        plt.plot([0, 100], [b, a * 100 + b])
+    # Enable plot animation
+    plt.ion()
+
+    # Draw data points
+    for data_point in dataset:
+        plt.scatter(data_point[0], data_point[1])
+
+    for i in range(0, epoch):
+        # Define default plot
+        plt.axis([0, 100, 0, 100])
+
+        # Draw hypothesis
+        ln, = plt.plot([0, 100], [b, a * 100 + b])
+
+        # Define cost
+        cost_a = 0
+        cost_b = 0
+        for sample in dataset:
+            temp_hypothesis = a * sample[0] + b
+            cost_a = cost_a + (temp_hypothesis - sample[1]) * sample[0]
+            cost_b += temp_hypothesis - sample[1]
+        cost_a /= len(dataset)
+        cost_b /= len(dataset)
+
+        # Update parameters
+        a -= learning_rate_a * cost_a
+        b -= learning_rate_b * cost_b
+
+        #  Wait before next hypothesis
+        plt.pause(0.01)
+
+        # Remove last hypothesis
+        ln.remove()
+
+    # Prevent program from ending
+    os.system("pause")
 
 
+# Import and transform an CSV file into a list
 def import_csv_data(filename):
     dataset = []
 
@@ -33,7 +66,6 @@ def import_csv_data(filename):
 
         for data_row in dataset_reader:
             row = [float(data) for data in data_row]
-            print(row)
             dataset.append(row)
             i += 1
 
